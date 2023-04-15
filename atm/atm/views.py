d5f0b7ced5405cf12f3238dd1bfeb5ecc3f72215
Laxman Gupta <laxman.gupta1006@gmail.com>
2023-04-16 03:39:56 +0530

    Added redirection logic

--- atm/atm/views.py
+++ atm/atm/views.py
@@ -5,3 +5,37 @@ def index(request):
     #current_time=datetime.now().strftime('%H:%M')
     return render(request, 'index.html')
 
+
+def banking(request):
+    #current_time=datetime.now().strftime('%H:%M')
+    return render(request, 'banking.html')
+
+
+def bankbalance(request):
+    #current_time=datetime.now().strftime('%H:%M')
+    return render(request, 'bankbalance.html')
+
+
+def changepin(request):
+    #current_time=datetime.now().strftime('%H:%M')
+    return render(request, 'changepin.html')
+
+def fingerprint(request):
+    #current_time=datetime.now().strftime('%H:%M')
+    return render(request, 'fingerprint.html')
+
+def enterpin(request):
+    #current_time=datetime.now().strftime('%H:%M')
+    return render(request, 'enterpin.html')
+
+def colorauth(request):
+    #current_time=datetime.now().strftime('%H:%M')
+    return render(request, 'colorauth.html')
+
+def ministatement(request):
+    #current_time=datetime.now().strftime('%H:%M')
+    return render(request, 'ministatement.html')
+
+def cashwithdrawal(request):
+    #current_time=datetime.now().strftime('%H:%M')
+    return render(request, 'ministatement.html')
